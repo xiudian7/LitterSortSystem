f50a5af2e3b6a2173a24e9db6c53a672fc0c2441
xiudian <1480997718@qq.com>
2024-04-30 15:27:59 +0800

    中间键插入和pom文件完善

--- sort_provider_server/sort-member-server/src/main/java/cn/litterSortSystem/xiudian7/member/MemberServer.java
+++ sort_provider_server/sort-member-server/src/main/java/cn/litterSortSystem/xiudian7/member/MemberServer.java
@@ -1,8 +1,19 @@
 package cn.litterSortSystem.xiudian7.member;
 
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@MapperScan("cn.litterSortSystem.xiudian7.mapper")
+@EnableDiscoveryClient
 public class MemberServer {
+
+        public static void main(String[] args) {
+            SpringApplication.run(MemberServer.class,args);
+        }
+
 }
