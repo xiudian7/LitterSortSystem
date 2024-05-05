f917a72bfc660bc8af6b9877df8f6794d29d3a34
xiudian <1480997718@qq.com>
2024-05-05 22:21:44 +0800

    daily commit

--- sort_provider_server/sort-identify-member/src/main/java/cn/litterSortSystem/xiudian7/identify/mapper/IdentifyMapper.java
+++
@@ -1,7 +0,0 @@
-package cn.litterSortSystem.xiudian7.identify.mapper;
-
-import cn.litterSortSystem.xiudian7.identify.ImageInfo;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface IdentifyMapper extends BaseMapper<ImageInfo> {
-}
--- sort_provider_server/sort-identify-member/src/main/java/cn/litterSortSystem/xiudian7/identify/service/IdentifyService.java
+++
@@ -1,8 +0,0 @@
-package cn.litterSortSystem.xiudian7.identify.service;
-
-import cn.litterSortSystem.xiudian7.identify.ImageInfo;
-import com.baomidou.mybatisplus.extension.service.IService;
-
-public interface IdentifyService extends IService<ImageInfo> {
-    public int identify(String imagePath);
-}
